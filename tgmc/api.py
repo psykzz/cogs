59a973edb5523635a7dc34f866a0b615faff5ddc
Matt Smith <psykzz@users.noreply.github.com>
2021-04-29 22:43:58 +0100

    Improve the API of getting winrate by gamemode

--- tgmc/api.py
+++ tgmc/api.py
@@ -34,6 +34,51 @@ class TGMC(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def get_winrate(gamemode=None):
+        raw_data = await http_get(
+            f"http://statbus.psykzz.com:8080/api/winrate?delta={delta}"
+        )
+        if not raw_data:
+            return await ctx.send(
+                "Unable to query data - check http://statbus.psykzz.com:8080 is online."
+            )
+
+        data = raw_data.get("by_gamemode", {}).get(gamemode, {}) if gamemode else data = raw_data.get("winrates", {})
+
+        winrates = discord.Embed()
+        winrates.type = "rich"
+
+        winrates.set_author(
+            name="TGMC Statbus", url=f"http://statbus.psykzz.com:8080",
+        )
+
+        result_type = [
+            "Marine Major Victory",
+            "Xenomorph Major Victory",
+            "Marine Minor Victory",
+            "Xenomorph Minor Victory",
+        ]
+        total_wins = 0
+        for res in result_type:
+            wins = data.get(res, 0)
+            total_wins += wins
+
+        xeno_wins = data.get("Xenomorph Major Victory", 0) + data.get(
+            "Xenomorph Minor Victory", 0
+        )
+        calc_winrates = round((xeno_wins / total_wins) * 100, 2)
+
+        winrates.add_field(
+            name="Winrate (Xenomorph to Marine)", value=f"{calc_winrates}%"
+        )
+        winrates.add_field(
+            name="View Raw",
+            value=f"http://statbus.psykzz.com:8080/api/winrate?delta={delta}",
+            inline=False,
+        )
+
+        await ctx.send(embed=winrates)
+
     @commands.group()
     async def winrates(self, ctx):
         "Check winrates from the API"
@@ -42,187 +87,25 @@ class TGMC(commands.Cog):
     @winrates.command()
     async def all(self, ctx, delta="14"):
         "Get the current winrates"
-
-        raw_data = await http_get(
-            f"http://statbus.psykzz.com:8080/api/winrate?delta={delta}"
-        )
-        if not raw_data:
-            return await ctx.send(
-                "Unable to query data - check http://statbus.psykzz.com:8080 is online."
-            )
-        data = raw_data.get("winrates", {})
-
-        winrates = discord.Embed()
-        winrates.type = "rich"
-
-        winrates.set_author(
-            name="TGMC Statbus", url=f"http://statbus.psykzz.com:8080",
-        )
-
-        result_type = [
-            "Marine Major Victory",
-            "Xenomorph Major Victory",
-            "Marine Minor Victory",
-            "Xenomorph Minor Victory",
-        ]
-        total_wins = 0
-        for res in result_type:
-            wins = data.get(res, 0)
-            total_wins += wins
-
-        xeno_wins = data.get("Xenomorph Major Victory", 0) + data.get(
-            "Xenomorph Minor Victory", 0
-        )
-        calc_winrates = round((xeno_wins / total_wins) * 100, 2)
-
-        winrates.add_field(
-            name="Winrate (Xenomorph to Marine)", value=f"{calc_winrates}%"
-        )
-        winrates.add_field(
-            name="View Raw",
-            value=f"http://statbus.psykzz.com:8080/api/winrate?delta={delta}",
-            inline=False,
-        )
-
-        await ctx.send(embed=winrates)
+        return await get_winrate(None) # None should get all winrates together
 
     @winrates.command()
     async def distress(self, ctx, delta="14"):
         "Get the current winrates on distress"
-
-        raw_data = await http_get(
-            f"http://statbus.psykzz.com:8080/api/winrate?delta={delta}"
-        )
-        if not raw_data:
-            return await ctx.send(
-                "Unable to query data - check http://statbus.psykzz.com:8080 is online."
-            )
-        data = raw_data.get("by_gamemode", {}).get("Distress Signal", {})
-
-        winrates = discord.Embed()
-        winrates.type = "rich"
-
-        winrates.set_author(
-            name="TGMC Statbus", url=f"http://statbus.psykzz.com:8080",
-        )
-
-        result_type = [
-            "Marine Major Victory",
-            "Xenomorph Major Victory",
-            "Marine Minor Victory",
-            "Xenomorph Minor Victory",
-        ]
-        total_wins = 0
-        for res in result_type:
-            wins = data.get(res, 0)
-            total_wins += wins
-
-        xeno_wins = data.get("Xenomorph Major Victory", 0) + data.get(
-            "Xenomorph Minor Victory", 0
-        )
-        calc_winrates = round((xeno_wins / total_wins) * 100, 2)
-
-        winrates.add_field(
-            name="Winrate (Xenomorph to Marine)", value=f"{calc_winrates}%"
-        )
-        winrates.add_field(
-            name="View Raw",
-            value=f"http://statbus.psykzz.com:8080/api/winrate?delta={delta}",
-            inline=False,
-        )
-
-        await ctx.send(embed=winrates)
+        return await get_winrate("Distress Signal")
+        
 
     @winrates.command()
     async def crash(self, ctx, delta="14"):
         "Get the current winrates on crash"
-
-        raw_data = await http_get(
-            f"http://statbus.psykzz.com:8080/api/winrate?delta={delta}"
-        )
-        if not raw_data:
-            return await ctx.send(
-                "Unable to query data - check http://statbus.psykzz.com:8080 is online."
-            )
-        data = raw_data.get("by_gamemode", {}).get("Crash", {})
-
-        winrates = discord.Embed()
-        winrates.type = "rich"
-
-        winrates.set_author(
-            name="TGMC Statbus", url=f"http://statbus.psykzz.com:8080",
-        )
-
-        result_type = [
-            "Marine Major Victory",
-            "Xenomorph Major Victory",
-            "Marine Minor Victory",
-            "Xenomorph Minor Victory",
-        ]
-        total_wins = 0
-        for res in result_type:
-            wins = data.get(res, 0)
-            total_wins += wins
-
-        xeno_wins = data.get("Xenomorph Major Victory", 0) + data.get(
-            "Xenomorph Minor Victory", 0
-        )
-        calc_winrates = round((xeno_wins / total_wins) * 100, 2)
-
-        winrates.add_field(
-            name="Winrate (Xenomorph to Marine)", value=f"{calc_winrates}%"
-        )
-        winrates.add_field(
-            name="View Raw",
-            value=f"http://statbus.psykzz.com:8080/api/winrate?delta={delta}",
-            inline=False,
-        )
-
-        await ctx.send(embed=winrates)
+        return await get_winrate("Crash")
 
     @winrates.command()
-    async def hunt(self, ctx, delta="14"):
+    async def bughunt(self, ctx, delta="14"):
         "Get the current winrates on bug hunt"
+        return await get_winrate("Bug Hunt")
 
-        raw_data = await http_get(
-            f"http://statbus.psykzz.com:8080/api/winrate?delta={delta}"
-        )
-        if not raw_data:
-            return await ctx.send(
-                "Unable to query data - check http://statbus.psykzz.com:8080 is online."
-            )
-        data = raw_data.get("by_gamemode", {}).get("Bug Hunt", {})
-
-        winrates = discord.Embed()
-        winrates.type = "rich"
-
-        winrates.set_author(
-            name="TGMC Statbus", url=f"http://statbus.psykzz.com:8080",
-        )
-
-        result_type = [
-            "Marine Major Victory",
-            "Xenomorph Major Victory",
-            "Marine Minor Victory",
-            "Xenomorph Minor Victory",
-        ]
-        total_wins = 0
-        for res in result_type:
-            wins = data.get(res, 0)
-            total_wins += wins
-
-        xeno_wins = data.get("Xenomorph Major Victory", 0) + data.get(
-            "Xenomorph Minor Victory", 0
-        )
-        calc_winrates = round((xeno_wins / total_wins) * 100, 2)
-
-        winrates.add_field(
-            name="Winrate (Xenomorph to Marine)", value=f"{calc_winrates}%"
-        )
-        winrates.add_field(
-            name="View Raw",
-            value=f"http://statbus.psykzz.com:8080/api/winrate?delta={delta}",
-            inline=False,
-        )
-
-        await ctx.send(embed=winrates)
+    @winrates.command()
+    async def huntparty(self, ctx, delta="14"):
+        "Get the current winrates on bug hunt"
+        return await get_winrate("Hunt party")
