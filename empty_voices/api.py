571b417cebb4498ce331d20c39788c1fb474e9bc
Matt <psykzz@users.noreply.github.com>
2024-10-08 00:10:01 +0100

    feat: add join leave support for voice

--- empty_voices/api.py
+++ empty_voices/api.py
@@ -7,6 +7,7 @@ IDENTIFIER = 1672261474290236288
 default_guild = {
     "emptyvoices": {
         "watchlist": [],
+        "temp_channels"
     },
 }
 
@@ -20,6 +21,45 @@ class EmptyVoices(commands.Cog):
 
         self.config.register_guild(**default_guild)
 
+
+    async def validate_channel(guild: discord.Guild, channel: discord.VoiceChannel):
+        "Check if this channel is empty, and delete it"
+        if len(channel.members) == 0:
+            await guild.owner.send("I should delete {channel.mention}, it's empty...")
+
+    async def validate_category(guild: discord.Guild, category: discord.CategoryChannel):
+        "Check if this category has an empty voice channel"
+        pass
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, after):
+        if await self.bot.cog_disabled_in_guild(self, member.guild):
+            return
+
+        watch_list = await guild_group.emptyvoices.watchlist()
+
+        guild = member.guild
+        if not guild:
+            return
+
+        channels = []
+        categories = []
+        if before.channel and before.channel.category in watch_list:
+            await guild.owner.send("watching!")
+            channels.append(before.channel)
+            categories.append(before.channel.category)
+        if after.channel and after.channel.category in watch_list:
+            await guild.owner.send("watching!")
+            channels.append(after.channel)
+            categories.append(after.channel.category)
+
+        for channel in channels:
+            validate_channel(guild, channel)
+
+        for category in categories:
+            validate_category(guild, category)
+
+    
     @commands.guild_only()
     @commands.group()
     async def emptyvoices(self, _ctx):
